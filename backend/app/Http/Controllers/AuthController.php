6290874d78f109c9b08a1e2a58f6896bf6c2e1c3
galuh305 <galuhsaputra305@gmail.com>
2026-04-07 10:46:57 +0700

    level

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
             'name' => $req->name,
             'email' => $req->email,
             'password' => Hash::make($req->password),
+            'role' => 'pegawai' // default
         ]);
 
         return response()->json($user);
